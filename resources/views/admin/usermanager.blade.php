28a643bba02fffa6d77e386fe8285eeef673b7c1
Niner1309 <sven0215@hotmail.com>
2016-11-16 18:25:21 +0100

    inital commit

---
+++ resources/views/admin/usermanager.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('style')
+    <!-- DataTables -->
+    <!--<link rel="stylesheet" href="../../plugins/datatables/dataTables.bootstrap.css">-->
+    {!! Html::Style('/plugins/datatables/dataTables.bootstrap.css') !!}
+@endsection
+
+
+@section('content')
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">Data Table With Full Features</h3>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+            <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Created at</th>
+                    <th>Updated at</th>
+
+                </tr>
+                </thead>
+                <tbody>
+
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->created_at }}</td>
+                        <td>{{ $user->updated_at }}</td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+        <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+@endsection
+
+@section('script')
+    <!-- DataTables -->
+    <!--
+    <script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="../../plugins/datatables/dataTables.bootstrap.min.js"></script>
+    -->
+    {!! Html::Script('plugins/datatables/jquery.dataTables.min.js') !!}
+    {!! Html::Script('plugins/datatables/dataTables.bootstrap.min.js') !!}
+    <!-- SlimScroll -->
+    <!--<script src="../../plugins/slimScroll/jquery.slimscroll.min.js"></script>-->
+    {!! Html::Script('plugins/slimScroll/jquery.slimscroll.min.js') !!}
+    <!-- FastClick -->
+    <!--<script src="../../plugins/fastclick/fastclick.js"></script>-->
+    {!! Html::Script('plugins/plugins/fastclick/fastclick.js') !!}
+    <!-- page script -->
+    <script>
+        $(function () {
+            $("#example1").DataTable();
+            $('#example2').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": false,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false
+            });
+        });
+    </script>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+use App\User;
+
+Route::get('/', function () {
+    return view('welcome')->render();
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('/usermanager', function () {
+
+    if(Auth::check() && Auth::user()->hasRole('admin')) {
+        $users = DB::table('users')->paginate();
+        $stuff = DB::select('SELECT us.name as username, ro.name as rolename FROM users AS us, user_has_roles AS r, roles AS ro WHERE r.user_id=us.id AND r.role_id=ro.id;');
+
+        var_dump($stuff[0]);
+        var_dump($stuff[1]);
+        return view('admin.usermanager', ['users' => $users])->render();
+    } else {
+        return redirect()->back();
+    }
+});
+
+Route::get('/run', function () {
+
+});
