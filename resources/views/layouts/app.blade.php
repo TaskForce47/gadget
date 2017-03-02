a4abfe2289102473c43f78f9ce0fc1cb37ebafb3
Niner1309 <sven0215@hotmail.com>
2017-03-02 17:05:08 +0100

    copyright and ignoring composer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -197,7 +197,7 @@
         <div class="pull-right hidden-xs">
             <b>Version</b> 0.1
         </div>
-        <strong>Copyright &copy; 2014-2016 <a href="http://armasim.de">Armasim</a>.</strong> All rights
+        <strong>Copyright &copy; 2014-2017 <a href="http://armasim.de">Armasim</a>.</strong> All rights
         reserved.
     </footer>
 
